17e07a94c9418991730fd6d3a28f810c521b859d
Josh Weeks <weeks.josh@gmail.com>
2022-09-19 23:59:19 -0400

    add client

---
+++ BeachBum.Core/Client/BeachBumApiClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+namespace BeachBum.Core.Client;
+
+public interface IBeachBumApiClient
+{
+    Task AddVenue(string name, string location, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Venue>> GetVenuesAsync(CancellationToken cancellationToken = default);
+}
+
+internal sealed class BeachBumApiClient : IBeachBumApiClient 
+{
+    public BeachBumApiClient(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    public async Task<IEnumerable<Venue>> GetVenuesAsync(CancellationToken cancellationToken = default)
+    {
+        using var client = _clientFactory.CreateClient();
+        return await client.GetFromJsonAsync<IEnumerable<Venue>>("/venues", cancellationToken) ?? Enumerable.Empty<Venue>();
+    }
+
+    public async Task AddVenue(string name, string location, CancellationToken cancellationToken = default)
+    {
+        var venue = new Venue()
+        {
+            Name = name,
+            Location = location
+        };
+        using var client = _clientFactory.CreateClient();
+        await client.PutAsJsonAsync("/venue", venue, cancellationToken);
+    }
+
+    private IHttpClientFactory _clientFactory;
+}
---
+++ BeachBum.Core/Client/ClientDi.cs
@@ -0,0 +1,17 @@
+﻿using BeachBum.Core.Client;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ClientDi
+{
+    public static IServiceCollection AddBeachBumClient(this IServiceCollection services, string baseUri)
+    {
+        services.AddHttpClient("BeachBum")
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri(baseUri));
+
+        services.AddTransient<IBeachBumApiClient, BeachBumApiClient>();
+
+        return services;
+    }
+
+}
